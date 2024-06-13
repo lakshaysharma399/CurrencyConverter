9cecadd8833c2fddc912283831751948b0674335
lakshaysharma399 <lakshaysharma399@gmail.com>
2024-06-13 13:47:25 +0530

    input component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,10 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import InputBox from './components/InputBox'
+import useCurrencyInfo from './hooks/useCurrencyinfo'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [amount, setAmount] = useState(0)
 
   return (
     <>
